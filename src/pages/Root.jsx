0205a2ab005da967e771b24a18543bcce651d552
sujk912 <aa991122aa@gmail.com>
2022-10-07 03:21:24 +0900

    feat: Add Root  page

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import useRouter from './../hooks/useRouter';
 
+// useRouter를 통해 push를 받아와 button이 눌리면 "/about"페이지로 이동하도록 push를 실행
 const Root = () => {
+    const { push } = useRouter();
     return (
-        <div>
+        <>
             <h1>Root</h1>
-            <a href="/about">About</a>
-        </div>
+            <button onClick={()=>push("/about")}>About</button>
+        </>
     )
 
 }
